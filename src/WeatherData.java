1519562af85f742eed105977a4f3111387a23598
terufumi1122 <terufumi1122@gmail.com>
2020-05-09 20:49:04 +0900

    java.util.Observer, java.util.Observableを使用してリファクタ

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,36 +1,16 @@
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
-public class WeatherData implements Subject {
-    private ArrayList observers;
+public class WeatherData extends Observable {
     private float temperature;
     private float humidity;
     private float pressure;
-    public WeatherData() {
-        observers = new ArrayList();
-    }
 
-    @Override
-    public void registerObserver(Observer o) {
-        observers.add(o);
-    }
-
-    @Override
-    public void removeObserver(Observer o) {
-        int i = observers.indexOf(o);
-        if (i >= 0) {
-            observers.remove(i);
-        }
-    }
-
-    @Override
-    public void notifyObservers() {
-        for (int i=0; i<observers.size(); i++) {
-            Observer observer = (Observer)observers.get(i);
-            observer.update(temperature, humidity, pressure);
-        }
-    }
+    public WeatherData() {}
 
     public void measurementsChanged() {
+        setChanged();
         notifyObservers();
     }
 
@@ -39,5 +19,17 @@ public class WeatherData implements Subject {
         this.humidity    = humidity;
         this.pressure    = pressure;
         measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
     }
 }
--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -4,8 +4,8 @@ public class WeatherStation {
 
         CurrentConditionsDisplay currentDisplay =
                 new CurrentConditionsDisplay(weatherData);
-        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+//        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+//        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(27, 54, 30.5f);
         weatherData.setMeasurements(25, 70, 33.5f);
